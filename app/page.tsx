5b26a26dc096ae3967e9f449e09a0de8206919b1
Clinton-Cochrane <clinton.edward.cochrane@gmail.com>
2026-01-24 00:00:47 -0800

    first draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,72 @@
+import featuredProjectsListView from "./featuredProjectsListView";
+import data from "./featuredProjects.json";
 import Image from "next/image";
+import headshot from "../public/headshot.jpeg"
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	const photosrc = "/../public/headshot.jpeg";
+	return (
+		<div>
+			<div id="hero_section" className="grid grid-cols-2">
+				<div id="left_half_hero_section">
+					<h1 id="name"> Clinton E. Cochrane</h1>
+					<h2 id="title">Software Engineer</h2>
+					<div id="buttons_hero_section">
+						<button id="email_me_button">Email Me</button>
+						<button id="view_projects_button">View Projects</button>
+					</div>
+				</div>
+				<div id="right_half_hero_section">
+					<Image width={100} height={100} alt="head shot" src={headshot} />
+				</div>
+			</div>
+
+			<div id="proof_strip_section">
+				<h2> Creative Problem Solving * 12 years of software development experience * Shipped Client Sites</h2>
+			</div>
+
+			<div id="featured_projects_section">{featuredProjectsListView(data.projects)}</div>
+
+			<div id="about_section">
+				<h6>Why do I like to build?</h6>
+				<p>
+					{" "}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae faucibus lacus, a aliquam sapien.
+					Curabitur in sagittis urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+					mus. Donec lobortis vestibulum lacus quis faucibus. Nunc elementum ut lacus eget congue. Praesent non rhoncus
+					velit. Quisque tincidunt vel nisi ut tincidunt. In ac mi aliquet, pharetra quam vitae, finibus turpis. Morbi
+					lacinia libero turpis, vel fringilla risus cursus id. Sed convallis nisi et fringilla sodales.
+				</p>
+				<h6>What do I like to build?</h6>
+				<p>
+					{" "}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae faucibus lacus, a aliquam sapien.
+					Curabitur in sagittis urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+					mus. Donec lobortis vestibulum lacus quis faucibus. Nunc elementum ut lacus eget congue. Praesent non rhoncus
+					velit. Quisque tincidunt vel nisi ut tincidunt. In ac mi aliquet, pharetra quam vitae, finibus turpis. Morbi
+					lacinia libero turpis, vel fringilla risus cursus id. Sed convallis nisi et fringilla sodales.
+				</p>
+				<h6>How do I like to build?</h6>
+				<p>
+					{" "}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae faucibus lacus, a aliquam sapien.
+					Curabitur in sagittis urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+					mus. Donec lobortis vestibulum lacus quis faucibus. Nunc elementum ut lacus eget congue. Praesent non rhoncus
+					velit. Quisque tincidunt vel nisi ut tincidunt. In ac mi aliquet, pharetra quam vitae, finibus turpis. Morbi
+					lacinia libero turpis, vel fringilla risus cursus id. Sed convallis nisi et fringilla sodales.
+				</p>
+				<h6>What do I want to do?</h6>
+				<p>
+					{" "}
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae faucibus lacus, a aliquam sapien.
+					Curabitur in sagittis urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+					mus. Donec lobortis vestibulum lacus quis faucibus. Nunc elementum ut lacus eget congue. Praesent non rhoncus
+					velit. Quisque tincidunt vel nisi ut tincidunt. In ac mi aliquet, pharetra quam vitae, finibus turpis. Morbi
+					lacinia libero turpis, vel fringilla risus cursus id. Sed convallis nisi et fringilla sodales.
+				</p>
+			</div>
+
+			<div id="contact_section"></div>
+		</div>
+	);
 }
